d876c43e69ef0ae49652e045c2bd5196ea57d22a
Ciel Baumann <ciel.baumann@gmail.com>
2026-05-03 15:46:59 +1200

    Debug prune

---
+++ run.py
@@ -0,0 +1,96 @@
+""" Computing Excitatory-Inhibitory Neurotransmitter Ratios of Discrete 
+Subnetworks of the Drosophila Connectome 
+
+Program Author: Ciel Baumann
+
+
+--- DATA
+
+Dataset: FlyWire Whole-brain Connectome Connectivity Data
+Dataset Retrieved From: https://zenodo.org/records/10676866
+Dataset Version: 783.0
+Dataset Published By: Flywire Consortium
+
+Data Files used:
+- proofread_connections_783.feather
+- proofread_root_ids_783.npy
+
+Dataset Citation (APA):
+FlyWire Consortium. (2024). FlyWire Whole-brain Connectome Connectivity Data 
+  (783.0) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.10676866
+  
+  
+--- USAGE
+
+TODO
+
+
+
+--- CONTENTS
+
+TODO
+
+
+"""
+
+import dask
+from dask import dataframe as ddf
+from queue import Queue
+
+
+### CLUSTER IDENTIFICATION SUPPORT FUNCTIONS ------------------------------
+
+def prune(df: ddf.DataFrame) -> ddf.DataFrame:
+    """ Iteratively remove degree 1 edges from a dask dataframe 
+    
+    A degree 1 edge is defined here as an edge associated with at least one
+    degree 1 node, where a degree 1 node is a node connected by any number of 
+    edges to one and only one other node. No nodes in the dataset will have 
+    edges to themselves. Synapse count and directionality are not considered.
+    
+    """
+    # Extract the edges as a set (in a dask bag)
+    edge_bag = df[["pre","post"]].to_bag()
+    
+    # Add (b,a) for every (a,b) in set to the set (makes it undirected)
+    edge_bag = edge_bag.map(lambda edge: [edge, (edge[1], edge[0])])
+    edge_bag = edge_bag.flatten().distinct()
+    
+    # Create dict where key is node id, and val is list of neighbour nodes.
+    # This makes it easy to track retained edges and degree simultaneously.
+    edge_bag_tuple_groups = edge_bag.foldby(key = lambda edge: edge[0],
+                                            binop = lambda accum, edge: accum + [edge[1]],
+                                            initial = [],
+                                            combine = lambda accum1, accum2: accum1 + accum2,
+                                            combine_initial = [])
+    edge_bag_dict = edge_bag_tuple_groups.map(lambda tup: {tup[0]: tup[1]})    
+    
+    deg1_queue = Queue() # Queue all nodes with only one neighbour
+    
+    while not deg1_queue.empty:
+        # Neighbour is None if deg1_node already pruned away, else [neighbour_id]
+        neighbour = edge_bag_dict.get(deg1_queue.pop(), None)
+        if neighbour:
+            neighbour = neighbour[0] # Neighbour ID is the only item in list
+            
+            # Remove deg1 node from neighbour's connections 
+            # (i.e.,remove neighbour -> deg1 edge)
+            edge_bag_dict[neighbour].remove(deg1_node)
+            
+            # If neighbour is now deg1, add neighbour to deg1 queue
+            if len(edge_bag_dict[neighbour]) == 1:
+                deg1_queue.put(neighbour)
+                
+            # Remove deg1 node key from dict (remove deg1 -> neighbour edge)
+            del edge_bag_dict[deg1_node]
+        
+    # Get expanded list of tuples of all edges post-pruning
+    def expand(edge_data):
+        key, target_list = edge_data[0], edge_data[1]
+        return target_list.map(lambda target: (key, target))
+    new_edge_bag = edge_bag_dict.map(lambda edge_data: expand(edge_data)).flatten()    
+    
+    # Intersect remaining edges with original dataframe to get pruned df    
+    new_edge_df = new_edge_bag.to_dataframe(meta={"pre": int, "post": int})
+    pruned_df = new_edge_df.merge(df, on=["pre","post"], how="inner")
+    return pruned_df
--- test.py
+++ test.py
@@ -1,104 +1,107 @@
 """ Cluster Detection Test Suite """
 
+import pandas as pd
 import unittest
-from dask import dataframe as ddf
 
-import main
+from dask import dataframe as ddf
+from pandas.testing import assert_frame_equal
+
+import run
 
 
 ### UNIT TESTS ------------------------------------------------------------
 
 class TestPrune(unittest.TestCase):
     
-    def base_case(self):
+    def test_base_case(self):
         """ A graph with no degree 1 edges returns itself """
         d = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}
         df_before = ddf.from_pandas(pd.DataFrame(data=d))
         df_after = ddf.from_pandas(pd.DataFrame(data=d))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
         
-    def single_incoming_deg1_edge(self):
+    def test_single_incoming_deg1_edge(self):
         d_b = {"pre": [0,1,3,3,5,0,6], "post": [1,2,2,4,4,5,0]}
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}        
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
         
-    def single_outgoing_deg1_edge(self):
+    def test_single_outgoing_deg1_edge(self):
         d_b = {"pre": [0,1,3,3,5,0,0], "post": [1,2,2,4,4,5,6]}
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}        
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
         
-    def two_deg1_edges_both_incoming(self):
+    def test_two_deg1_edges_both_incoming(self):
         d_b = {"pre": [0,1,3,3,5,0,6,7], "post": [1,2,2,4,4,5,0,4]}        
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)        
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
     
-    def two_deg1_edges_both_outgoing(self):
+    def test_two_deg1_edges_both_outgoing(self):
         d_b = {"pre": [0,1,3,3,5,0,0,4], "post": [1,2,2,4,4,5,6,7]}        
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)        
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
     
-    def two_deg1_edges_one_in_one_out(self):
+    def test_two_deg1_edges_one_in_one_out(self):
         d_b = {"pre": [0,1,3,3,5,0,6,4], "post": [1,2,2,4,4,5,0,7]}        
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)        
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
     
-    def two_deg1_edges_one_out_one_in(self):
+    def test_two_deg1_edges_one_out_one_in(self):
         d_b = {"pre": [0,1,3,3,5,0,0,7], "post": [1,2,2,4,4,5,6,4]}        
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
         
-    def two_deg1_edges_both_incoming_2(self):
+    def test_two_deg1_edges_both_incoming_2(self):
         d_b = {"pre": [0,1,3,3,5,0,6,7], "post": [1,2,2,4,4,5,0,0]}
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]} 
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)        
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
     
-    def two_deg1_edges_both_outgoing_2(self):
+    def test_two_deg1_edges_both_outgoing_2(self):
         d_b = {"pre": [0,1,3,3,5,0,0,0], "post": [1,2,2,4,4,5,6,7]}
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)        
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
     
-    def two_deg1_edges_one_in_one_out_2(self):
+    def test_two_deg1_edges_one_in_one_out_2(self):
         d_b = {"pre": [0,1,3,3,5,0,0,7], "post": [1,2,2,4,4,5,6,0]}
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)        
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
     
-    def two_deg1_edges_one_out_one_in_2(self):
+    def test_two_deg1_edges_one_out_one_in_2(self):
         d_b = {"pre": [0,1,3,3,5,0,6,0], "post": [1,2,2,4,4,5,0,7]}
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)        
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
     
-    def combo_case(self):
+    def test_combo_case(self):
         d_b = {"pre": [0,1,3,3,5,0,0,6,7,9,9,10,1,13,14,
                        15,15,17,18,18,20,4,21,22,24,25], 
                "post": [1,2,2,4,4,5,6,7,8,8,10,11,12,1,2,
@@ -106,8 +109,8 @@ class TestPrune(unittest.TestCase):
         d_a = {"pre": [0,1,3,3,5,0], "post": [1,2,2,4,4,5]}
         df_before = ddf.from_pandas(pd.DataFrame(data=d_b))
         df_after = ddf.from_pandas(pd.DataFrame(data=d_a))
-        result = main.prune(df_before).compute()
-        self.assertEqual(df_after, result)
+        result = run.prune(df_before).compute()
+        assert_frame_equal(result, df_after.compute())
 
 
 class TestGetUpperThreshold(unittest.TestCase):

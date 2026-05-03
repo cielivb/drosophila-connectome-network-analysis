0f0ac4d979084920ac29d34a0b72985af55a0fb3
Ciel Baumann <ciel.baumann@gmail.com>
2026-05-03 17:41:24 +1200

    Add degree 1 nodes to queue

--- run.py
+++ run.py
@@ -49,9 +49,7 @@ def prune(df: ddf.DataFrame) -> ddf.DataFrame:
     edges to themselves. Synapse count and directionality are not considered.
     
     """
-    # Extract the edges as a set (in a dask bag)
     edge_bag = df[["pre","post"]].to_bag()
-    
     # Add (b,a) for every (a,b) in set to the set (makes it undirected)
     edge_bag = edge_bag.map(lambda edge: [edge, (edge[1], edge[0])])
     edge_bag = edge_bag.flatten().distinct()
@@ -65,25 +63,35 @@ def prune(df: ddf.DataFrame) -> ddf.DataFrame:
                                             combine_initial = [])
     edge_bag_dict = edge_bag_tuple_groups.map(lambda tup: {tup[0]: tup[1]})    
     
-    deg1_queue = Queue() # Queue all nodes with only one neighbour
+    # Find initial degree 1 nodes
+    deg1_queue = Queue()    
+    deg1_nodes = edge_bag_dict.map(
+        lambda edge_data: next(iter(edge_data)) # Map single dicts to keys (nodes)
+    ).filter(
+        lambda node: len(edge_bag_dict[node]) == 1 # Keep degree 1 nodes
+    )
     
+    # Queue all degree 1 nodes
+    # This is a bit hacky (using private vars) but avoids a for loop
+    # Idea from https://www.py4u.org/blog/python-putting-list-items-in-a-queue/#3-better-code-practices-for-optimization
+    with deg1_queue.mutex: # with queue lock
+        deg1_queue.queue.extend(deg1_nodes.compute()) # Add all at once
+    
+    # Iteratively prune away degree 1 nodes from edge_bag_dict   
     while not deg1_queue.empty:
         # Neighbour is None if deg1_node already pruned away, else [neighbour_id]
         neighbour = edge_bag_dict.get(deg1_queue.pop(), None)
         if neighbour:
             neighbour = neighbour[0] # Neighbour ID is the only item in list
-            
             # Remove deg1 node from neighbour's connections 
             # (i.e.,remove neighbour -> deg1 edge)
             edge_bag_dict[neighbour].remove(deg1_node)
-            
             # If neighbour is now deg1, add neighbour to deg1 queue
             if len(edge_bag_dict[neighbour]) == 1:
                 deg1_queue.put(neighbour)
-                
             # Remove deg1 node key from dict (remove deg1 -> neighbour edge)
             del edge_bag_dict[deg1_node]
-        
+            
     # Get expanded list of tuples of all edges post-pruning
     def expand(edge_data):
         """ e.g., edge_data = {0: [1, 5]} -> [(0,1), (0,5)] """
